80b6d3ff4b4a1b2ea664c767c12969c94ddf50da
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-24 23:15:17 -0300

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,8 +3,32 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { rmdir } = require('fs');
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
 module.exports = (on, config) => {
     allureWriter(on, config);
     return config;
-};
+
+    on('task', {
+        deleteFolder(screenshots) {
+            console.log('deleting folder %s', screenshots);
+
+            return new Promise((resolve, reject) => {
+                rmdir('cypress/screenshots', { maxRetries: 10, recursive: true }, (err) => {
+                    if (err) {
+                        console.error(err);
+
+                        return reject(err);
+                    }
+
+                    resolve(null);
+                })
+            })
+        },
+    })
+}
