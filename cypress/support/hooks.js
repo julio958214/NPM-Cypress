9dc8676b58d8b26740e3cb7ef8ee1054bc31d62b
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-25 01:02:24 -0300

    Update hooks.js

--- cypress/support/hooks.js
+++ cypress/support/hooks.js
@@ -15,5 +15,5 @@ afterEach(function () {
     if(this.currentTest.state === 'failed')
         return cy.wait(1000).screenshot('error/error');
     else 
-        return cy.wait(1000).screenshot('output/success');
+        return cy.wait(1500).screenshot('output/success');
 });
