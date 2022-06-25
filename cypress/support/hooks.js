3d73d23306c63945de7c278fc47b0d7f3fcfafd6
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-24 23:16:29 -0300

    Update hooks.js

--- cypress/support/hooks.js
+++ cypress/support/hooks.js
@@ -6,6 +6,11 @@ beforeEach(() => {
     cy.visit(Cypress.env('baseUrl'));
 });
 
+beforeEach(function () { 
+    if(this.currentTest.state = true)
+        cy.task('deleteFolder');
+});
+
 afterEach(function () {
     if(this.currentTest.state === 'failed')
         return cy.wait(1000).screenshot('error/error');
