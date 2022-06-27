6274735408092543af851048131e3a618df0baac
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-27 19:40:54 -0300

    Update hooks.js

--- cypress/support/hooks.js
+++ cypress/support/hooks.js
@@ -6,13 +6,13 @@ beforeEach(() => {
     cy.visit(Cypress.env('baseUrl'));
 });
 
-before(function () { 
-     cy.task('deleteFolder', 'cypress/screenshots');
+before(function () {
+    cy.task('pathScreenshot', 'cypress/screenshots');        
 });
 
 afterEach(function () {
-    if(this.currentTest.state === 'failed')
+    if (this.currentTest.state === 'failed')
         return cy.wait(1500).screenshot('error/error');
-    else 
+    else
         return cy.wait(1500).screenshot('output/success');
 });
